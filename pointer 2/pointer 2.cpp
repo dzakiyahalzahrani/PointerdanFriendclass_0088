46829fc99fcdf9b5857f7ad24c0f989cf1df52e8
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-05-06 15:21:07 +0700

    ponter 2 selesai

--- pointer 2/pointer 2.cpp
+++ pointer 2/pointer 2.cpp
@@ -1,3 +1,19 @@
 //pointer 2
 #include <iostream>
 using namespace std;
+class mahasiswa {
+public:
+	int nim;
+	void showNim()
+	{
+		cout << "No Induk =" << nim << endl;
+	}
+};
+int main()
+{
+	mahasiswa* mhs = new mahasiswa{ 1 }; 
+	mhs->nim = 2;
+	mhs->showNim();
+	delete mhs;
+	return 0;
+ }
